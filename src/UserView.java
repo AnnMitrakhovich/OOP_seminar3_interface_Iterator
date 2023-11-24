ddafbd0ddc05179e4366d03353314806211c3229
Ann <ann.mitrakhovich@gmail.com>
2023-11-24 12:57:57 +0300

    copy class user

---
+++ src/UserView.java
@@ -0,0 +1,21 @@
+import java.util.logging.Logger;
+
+import java.util.List;
+
+public class UserView {
+    Logger logger = Logger.getLogger(UserView.class.getName());
+
+    public void sendOnConsole(List<User> userList){
+        for(User user: userList){
+            logger.info(user.toString());
+        }
+    }
+
+    public void sendOnConsoleUserGroup(StudentGroup studentGroup){
+        logger.info(studentGroup.toString());
+    }
+
+    public void sendOnConsoleListStudent(List<Student> students){
+        logger.info(students.toString());
+    }
+}
